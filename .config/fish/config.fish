32d5585a2a1715026ee1c6cfec4f811525b4c9cf
Tomás Farías Santana <tomas@tomasfarias.dev>
2025-08-21 02:04:07 +0200

    chore: Fish updates

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,15 +2,22 @@ if test -d "$HOME/.local/bin"
    fish_add_path "$HOME/.local/bin"
 end
 
-if [ $XDG_SESSION_TYPE = "wayland" ]
+if test -n "$XDG_SESSION_TYPE"
+and test "$XDG_SESSION_TYPE" = "wayland"
     set MOZ_ENABLE_WAYLAND 1
     set QT_QPA_PLATFORM wayland
     set ANKI_WAYLAND 1
 end
 
 set -g fish_greeting
-set EDITOR "emacsclient -t -a \"emacs -nw\""
+set EDITOR "emacsclient -nw -a \"emacs -nw\""
+set GIT_EDITOR "emacsclient -nw -a \"emacs -nw\""
 set GPG_TTY $(tty)
+set TERM xterm-24bit
+set LC_ALL en_US.UTF-8
+set LANG en_US.UTF-8
+set LANGUAGE en_US.UTF-8
+set LSP_USE_PLISTS true
 
 # Rust paths
 fish_add_path "$HOME/.cargo/bin"
@@ -40,12 +47,13 @@ end
 # Start prompt
 if status is-interactive
    starship init fish | source
+   fzf --fish | source
 end
 
 # Ensure SSH agent
 fish_ssh_agent
 
-# ASDF
+# Mise
 if status is-interactive
-    source /opt/asdf-vm/asdf.fish
+    mise activate fish | source
 end
